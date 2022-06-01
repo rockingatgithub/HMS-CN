f61d5ac7039148523db372758c6fbbe3a430232f
rockingatgithub <contactwithsid@gmail.com>
2022-06-01 20:27:36 +0530

    added appointment book and edit status

--- controllers/appointment/index.js
+++ controllers/appointment/index.js
@@ -12,3 +12,31 @@ module.exports.bookAppointment = async (req, res) => {
     })
 
 }
+
+
+module.exports.getAppointment = async (req, res) => {
+
+    console.log(req.params);
+
+    let appointment = await Appointment.findById(req.params.apId).populate('doctor').populate('patient')
+
+    return res.json({
+        message: "Appointment booked successfully!",
+        appointment
+    })
+
+}
+
+module.exports.changeAppointment = async (req, res) => {
+
+    console.log(req.params, req.query);
+
+    let appointment = await Appointment.findOneAndUpdate({ _id : req.params.apId}, {  status: req.query.status  }).populate('doctor').populate('patient')
+
+    return res.json({
+        message: "Appointment changed successfully!",
+        appointment
+    })
+
+
+}
--- routes/appoint/index.js
+++ routes/appoint/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { bookAppointment } = require('../../controllers/appointment');
+const { bookAppointment, getAppointment, changeAppointment } = require('../../controllers/appointment');
 const router = express.Router();
 
-router.post('/book', bookAppointment)
+router.post('/book', bookAppointment);
+router.get('/fetch/:apId', getAppointment);
+router.patch('/setStatus/:apId', changeAppointment)
 
 
 module.exports = router;
